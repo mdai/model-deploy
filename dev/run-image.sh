86d234ee4c349753e2678e415e1cb28322496467
Mohammed Affan <affanmd@nyu.edu>
2020-02-20 15:37:46 +0000

    Expose docker port to probe healthz and ready [ch885]

--- dev/run-image.sh
+++ dev/run-image.sh
@@ -2,4 +2,5 @@
 
 docker stop model-dev
 docker rm model-dev
-docker run -d --network=mdai-dev --name=model-dev model-dev:latest
+docker run -d --network=mdai-dev --name=model-dev -p 6324:6324 model-dev:latest
+echo "Server listening on localhost port 6324"
