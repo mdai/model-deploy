fd08db83688c4f6499f42210da10959a8de8c1e3
Mohammed Affan <affanmd@nyu.edu>
2020-03-28 15:31:37 +0000

    delete old files

--- dev/build-image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-TARGET_DIR=$1
-
-$DIR/../scripts/build-image.py --docker_env py37 --image_name model-dev:latest --target_folder $TARGET_DIR
--- dev/run-image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker stop model-dev
-docker rm model-dev
-docker run -d --network=mdai-dev --name=model-dev -p 6324:6324 model-dev:latest
-echo "Server listening on localhost port 6324"
